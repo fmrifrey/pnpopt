9f0012a5eefcb9903f4c4154d476c8339e5b9219
fmrifrey <djfrey@umich.edu>
2024-09-27 11:14:49 -0400

    changed Wavelet type to db4 with adjustable n_levels

--- +reg/WavL1.m
+++ +reg/WavL1.m
@@ -2,40 +2,45 @@ classdef WavL1
 
     properties
         lam % lagrange multiplier for L1 regularization
+        n_lvl % number of levels for wavelet dec
     end
     
     methods
-        function obj = WavL1(lam)
+        function obj = WavL1(lam,n_lvl)
             % constructor function for Wavelet L1 regularizer
             if nargin < 1 || isempty(lam)
                 lam = 1; % default lambda is 1
             end
             obj.lam = lam;
+        
+            if nargin < 2 || isempty(n_lvl)
+                n_lvl = 3;
+            end
+            obj.n_lvl = n_lvl;
         end
         
         function x_p = prox(obj, x)
             % compute the DWT
-            [ca,cd] = dwt(x(:),'haar');
+            [C,L] = wavedec(x(:),obj.n_lvl,'db4');
 
             % soft thresholding operation (L1 proximal operator)
             l1_prox = @(x) sign(x) .* max(abs(x) - obj.lam, 0);
 
             % apply l1 thresholding
-            ca = l1_prox(ca);
-            cd = l1_prox(cd);
+            C = l1_prox(C);
         
             % inverse DWT to reconstruct x
-            x_p = reshape(idwt(ca,cd,'haar'),size(x));
+            x_p = reshape(waverec(C,L,'db4'),size(x));
 
         end
 
         function n = norm(obj, x)
             % compute the DWT
-            [ca,cd] = dwt(x(:),'haar');
+            C = wavedec(x(:),obj.n_lvl,'db4');
 
             % computes the L1 norm of x
             % i.e. ||Wx||_1
-            n = obj.lam*(sum(abs(ca)+abs(cd)));
+            n = obj.lam*(sum(abs(C(:))));
         end
 
     end
